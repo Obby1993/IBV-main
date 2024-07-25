688d2ba8c6511150d116eb166dd445470c6d78d4
Obby1993 <olivier.barthelemy1993@gmail.com>
2024-07-25 09:00:49 +0200

    Webhook stripe:  sans config: bordy parse false

--- app/stripe/route.ts
+++ app/stripe/route.ts
@@ -1,13 +1,10 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import { buffer } from 'micro';
+import { NextRequest, NextResponse } from 'next/server';
 import Stripe from 'stripe';
-import { stripe } from '../../../lib/stripe';
-import prisma from '../../../lib/prisma'; // Ajustez le chemin selon votre configuration Prisma
+import { stripe } from '../../lib/stripe';
+import prisma from '../../lib/prisma';
 
 export const config = {
-  api: {
-    bodyParser: false, // Désactiver le bodyParser intégré de Next.js pour traiter les requêtes Stripe
-  },
+  runtime: 'edge',
 };
 
 // Type guard pour vérifier que le client est un Customer et non un DeletedCustomer
@@ -15,73 +12,67 @@ function isCustomer(customer: Stripe.Customer | Stripe.DeletedCustomer): custome
   return (customer as Stripe.Customer).metadata !== undefined;
 }
 
-const webhookHandler = async (req: NextApiRequest, res: NextApiResponse) => {
-  if (req.method === 'POST') {
-    const buf = await buffer(req);
-    const sig = req.headers['stripe-signature']!;
+export async function POST(req: NextRequest) {
+  const sig = req.headers.get('stripe-signature');
 
-    let event: Stripe.Event;
-
-    try {
-      event = stripe.webhooks.constructEvent(buf, sig, process.env.STRIPE_WEBHOOK_SECRET!);
-    } catch (err) {
-      console.error('Webhook signature verification failed.');
-      if (err instanceof Error) {
-        return res.status(400).send(`Webhook Error: ${err.message}`);
-      } else {
-        return res.status(400).send('Webhook Error: An unknown error occurred.');
-      }
-    }
-
-    // Handle the event
-    if (event.type === 'checkout.session.completed') {
-      const session = event.data.object as Stripe.Checkout.Session;
-      if (session.customer) {
-        const customer = await stripe.customers.retrieve(session.customer as string);
-
-        if (isCustomer(customer) && customer.metadata.ibvID) {
-          const ibvID = customer.metadata.ibvID;
-
-          // Mettre à jour la base de données ici
-          try {
-            const player = await prisma.player.update({
-              where: { id: ibvID },
-              data: {
-                paiement: true,
-                stripeCustomerId: session.customer as string,
-              },
-            });
-             // Ajouter le joueur à l'événement
-             await prisma.event.update({
-              where: { id: player.eventId },
-              data: {
-                players: {
-                  connect: { id: player.id },
-                },
-              },
-            });
-          } catch (err) {
-            console.error('Error updating player record:', err);
-            return res.status(500).send('Server error');
-          }
-
-          return res.status(200).json({ received: true });
-        } else {
-          console.error('ibvID is missing in customer metadata or customer is not a valid Customer');
-          return res.status(400).json({ error: 'ibvID is missing in customer metadata or customer is not a valid Customer' });
-        }
-      } else {
-        console.error('Customer is null in session:', session);
-        return res.status(400).json({ error: 'Customer is null in session' });
-      }
-    }
-
-    // Retourner une réponse 200 pour indiquer à Stripe que le webhook a été reçu correctement
-    return res.json({ received: true });
-  } else {
-    res.setHeader('Allow', 'POST');
-    res.status(405).end('Method Not Allowed');
+  if (!sig) {
+    return new NextResponse('Missing stripe-signature header', { status: 400 });
   }
-};
 
-export default webhookHandler;
+  let event: Stripe.Event;
+
+  try {
+    const buf = await req.arrayBuffer();
+    const textDecoder = new TextDecoder();
+    const str = textDecoder.decode(buf);
+    event = stripe.webhooks.constructEvent(Buffer.from(str), sig, process.env.STRIPE_WEBHOOK_SECRET!);
+  } catch (err) {
+    console.error('Webhook signature verification failed.');
+    return new NextResponse(`Webhook Error: ${err instanceof Error ? err.message : 'Unknown error'}`, { status: 400 });
+  }
+
+  if (event.type === 'checkout.session.completed') {
+    const session = event.data.object as Stripe.Checkout.Session;
+    if (session.customer) {
+      const customer = await stripe.customers.retrieve(session.customer as string);
+
+      if (isCustomer(customer) && customer.metadata.ibvID) {
+        const ibvID = customer.metadata.ibvID;
+
+        try {
+          // Récupérer le joueur
+          const player = await prisma.player.update({
+            where: { id: ibvID },
+            data: {
+              paiement: true,
+              stripeCustomerId: session.customer as string,
+            },
+          });
+
+          // Ajouter le joueur à l'événement
+          await prisma.event.update({
+            where: { id: player.eventId },
+            data: {
+              players: {
+                connect: { id: player.id },
+              },
+            },
+          });
+
+        } catch (err) {
+          console.error('Error updating player record or event:', err);
+          return new NextResponse('Server error', { status: 500 });
+        }
+
+        return new NextResponse(JSON.stringify({ received: true }), { status: 200 });
+      } else {
+        console.error('ibvID is missing in customer metadata or customer is not a valid Customer');
+        return new NextResponse('ibvID is missing in customer metadata or customer is not a valid Customer', { status: 400 });
+      }
+    }
+  } else {
+    console.warn(`Unhandled event type ${event.type}`);
+  }
+
+  return new NextResponse(JSON.stringify({ received: true }), { status: 200 });
+}
